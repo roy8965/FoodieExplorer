0289275a86443a80649fc201c7e45c8285cc0baa
Roy <roan@Roys-MacBook-Pro.local>
2019-11-02 12:56:21 -0400

    app layout template

---
+++ FoodieExplorer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodieExplorer
+//
+//  Created by Roy on 11/2/19.
+//  Copyright © 2019 Roy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
